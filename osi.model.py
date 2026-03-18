434fcb89a07245c1d81b6de08e752ee13147b1b2
David H <93612597+Dhurtado106@users.noreply.github.com>
2026-03-17 19:47:30 -0700

    Add files via upload

---
+++ osi.model.py
@@ -0,0 +1,8 @@
+print("7 - | Application | Where humans process data and information")
+print("6 - | Presentation | Ensures data is in a usable format")
+print("5 - | Session | Capable of maintaining connections")
+print("4 - | Transport | Data is forwarded to a service capable of handling requests")
+print("3 - | Network Layer | Responsible for which path packets should travel on a network")
+print("2 - | Data Link | Responsible for which physical devices packets should go to")
+print("1 - | Physical | The physical infrastructure to transport data")
+
